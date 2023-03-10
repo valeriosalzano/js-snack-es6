50fcc0c3522e62efa2e407a8257459297d921a5e
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-10 17:37:54 +0100

    snack 2 conclusion, working on snack 3

--- js/main.js
+++ js/main.js
@@ -37,22 +37,56 @@ Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
 
+const snack2ContainerDom = document.getElementById('snack2-container');
+
+// creo un array con i nomi delle squadre
+const teamNames = ["Atalanta","Bologna","Cremonese","Empoli","Fiorentina","Inter","Juventus","Lazio","Lecce","Milan","Monza","Napoli","Roma","Salernitana","Sampdoria","Sassuolo","Spezia","Torino","Udinese","Verona"];
+
+// popolo l'array con le squadre che avranno name, points, fouls.
+const soccerTeams = teamNames.map( name => ({name, points : 0, fouls : 0 }));
+
+soccerTeams.forEach(team => {
+    team.points = randomNumber(0,10);
+    team.fouls = randomNumber(0,5);
+})
+
+// stampo la lista delle squadre con i relativi falli subiti
+printP(snack2ContainerDom,`Ecco la lista delle squadre con relativi falli subiti:`)
+printList(snack2ContainerDom,...soccerTeams.map(team => `${team.name}. Falli subiti: ${team.fouls}`));
+
+// Snack 2 EXTRA 
+// cerco la squadra con il punteggio migliore
+const maxPoints = Math.max(...soccerTeams.map(team => team.points));
+
+printP(snack2ContainerDom,`Il punteggio massimo rilevato è: ${maxPoints}.`);
+
+// ricavo i team con il punteggio migliore e ne stampo la lista
+let winnerTeams = soccerTeams.filter( team => team.points == maxPoints );
+printList(snack2ContainerDom, ...winnerTeams.map(team => `${team.name}`));
+
 // Snack 3
 /*
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 */
 
+
+
+// LISTA FUNZIONI
+
+// funzione che restituisce un numero random comprenso tra min e max
 function randomNumber (min,max){
     return Math.floor(Math.random()*(max-min+1))+min;
 }
 
-function printP (domLink, ...contents){
+// funzione che stampa sul DOM un 'p' che include diversi "contents"
+function printP (domLink,...contents){
     const newP = document.createElement('p');
     newP.innerHTML = contents;
     domLink.append(newP);
 }
 
+// funzione che stampa sul DOM una lista che include diversi "listItems"
 function printList (domLink,...listItems){
     const newList = document.createElement('ul');
     listItems.forEach(liContent => {
